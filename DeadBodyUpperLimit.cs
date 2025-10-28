f180e71e8fc3eb7ebb715f26d4fac6cd88cb0e97
findsky6544 <36687288+findsky6544@users.noreply.github.com>
2025-10-29 00:45:51 +0800

    v1.0

---
+++ DeadBodyUpperLimit.cs
@@ -0,0 +1,76 @@
+﻿using Duckov.Rules;
+using System.IO;
+using System.Reflection;
+using System;
+using Duckov.Scenes;
+using UnityEngine.SceneManagement;
+using HarmonyLib;
+using Duckov;
+using System.Collections.Generic;
+using Duckov.UI;
+using Duckov.Quests;
+using UnityEngine;
+
+namespace DeadBodyUpperLimit
+{
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        public static int SaveDeadbodyCount = 1;
+
+        private Harmony harmony;
+
+        private string id = "com.findsky.DeadBodyUpperLimit";
+        private void OnEnable()
+        {
+            try
+            {
+                Console.WriteLine("onEnable");
+                SaveDeadbodyCount = LoadDataFromFile(1);
+                harmony = new Harmony(id);
+                harmony.PatchAll();
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void OnDisable()
+        {
+            Console.WriteLine("OnDisable");
+
+            harmony.UnpatchAll(id);
+        }
+        public static int LoadDataFromFile(int defaultValue)
+        {
+            Console.WriteLine("LoadDataFromFile");
+            string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (directoryName != null)
+            {
+                string path = Path.Combine(directoryName, "DeadBodyUpperLimit_Data.txt");
+                if (File.Exists(path))
+                {
+                    using (StreamReader streamReader = new StreamReader(path))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            string text = streamReader.ReadLine();
+                            if (!string.IsNullOrEmpty(text))
+                            {
+                                int num;
+                                if (int.TryParse(text, out num))
+                                {
+                                        return num;
+                                }
+                                return defaultValue;
+                            }
+                        }
+                        return defaultValue;
+                    }
+                }
+                File.Create(path).Close();
+            }
+            return defaultValue;
+        }
+
+    }
+}
---
+++ Patch.cs
@@ -0,0 +1,33 @@
+﻿using Duckov;
+using Duckov.Rules;
+using Duckov.UI;
+using HarmonyLib;
+using ItemStatsSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeadBodyUpperLimit
+{
+
+    [HarmonyPatch(typeof(Ruleset), "SaveDeadbodyCount", MethodType.Getter)]
+    public class Patch_Ruleset_SaveDeadbodyCount
+    {
+        static bool Prefix(Ruleset __instance, ref int __result)
+        {
+            Console.WriteLine("Patch_Ruleset_SaveDeadbodyCount");
+            __result = ModBehaviour.SaveDeadbodyCount;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(DeadBodyManager), "OnSubSceneLoaded")]
+    public class Patch_DeadBodyManager_OnSubSceneLoaded
+    {
+        static bool Prefix(DeadBodyManager __instance)
+        {
+            Console.WriteLine("Patch_DeadBodyManager_OnSubSceneLoaded");
+            return true;
+        }
+    }
+}
